16516270d294ecfe0ccb2988877933ff92bd3e51
Yū Kobayashi <kb10uy@kb10uy.org>
2022-12-20 22:05:58 +0900

    Remove unused imports

--- scripts/list-blocks.py
+++ scripts/list-blocks.py
@@ -1,12 +1,3 @@
-# East Asian Width Checker
-# F  (Fullwidth) "FULLWIDTH" chars; ＡＢＣ
-# H  (Halfwidth) "HALFWIDTH" chars; ｱｲｳ
-# W  (Wide)      Wide chars;        あア漢
-# Na (Narrow)    Narrow chars;      ABC
-# A  (Ambiguous) Ambiguous chars;   ↑↓←→
-# N  (Neutral)   Neutral chars;     Áς
-
-from unicodedata import east_asian_width, category
 import sys
 from mmtool.unicode import find_block
 import fontforge
@@ -23,11 +14,9 @@ for glyph in font.glyphs():
     codepoint = glyph.unicode
     char = chr(codepoint)
     block = find_block(codepoint)
-    eaw = east_asian_width(char)
     glyph_width = glyph.width / base_em
 
     found_blocks.add(block)
-    # print(f"U+{codepoint:06X} '{char}' [{eaw}] => {block}")
 
 print("Unicode Blocks which the Font have:")
 for _, _, block in sorted(found_blocks):
