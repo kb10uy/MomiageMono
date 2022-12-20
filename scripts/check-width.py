e7e0eb5e93069a4d53203a91356328f8974439bf
Yū Kobayashi <kb10uy@kb10uy.org>
2022-12-21 02:03:28 +0900

    Just compare glyph and EAW

--- scripts/check-width.py
+++ scripts/check-width.py
@@ -14,26 +14,27 @@ for glyph in font.glyphs():
     str_expr = f"U+{codepoint:06X} '{chr(codepoint)}'"
     _, _, block_name = unicode_block_of(codepoint)
 
-    block_width = block_width_of(block_name)
+    block_info = block_width_of(block_name)
     target_width = target_width_of(codepoint)
     glyph_width = "full" if glyph.width / base_em > 0.95 else "half"
 
-    if block_width is None:
+    if block_info is None:
         continue
+    block_width, block_font_tag = block_info
 
-    if block_width[0] == "complex":
+    if block_width == "complex":
         # print(f"[I] {str_expr} has complex width, skipping")
         continue
 
-    if block_width[1] is not None and block_width[1] != font_tag:
+    if block_font_tag is not None and block_font_tag != font_tag:
         # print(f"[I] {str_expr} will not be copied, skipping")
         continue
 
-    if not (block_width[0] == target_width and block_width[0] == glyph_width):
+    if target_width != glyph_width:
         print(f"[E] {str_expr} has different width")
-        print(f"    Unicode Block   : {block_width[0]} (from {block_name})")
-        print(f"    East Asian Width: {target_width}")
-        print(f"    Glyph           : {glyph_width}")
+        print(f"    Unicode Block    : {block_name}")
+        print(f"    East Asian Width : {target_width}")
+        print(f"    Glyph            : {glyph_width}")
         continue
 
     # print(f"[I] {str_expr} has consistent width: {block_width}")
