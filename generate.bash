f3901fadbebbeb325cfc240744d52ac0fa8896a8
Yū Kobayashi <kb10uy@kb10uy.org>
2022-12-22 02:20:56 +0900

    Improve generate.bash

--- generate.bash
+++ generate.bash
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
+set -e
+
+rm -rf dist/*
 
 python3 "scripts/momiage-mono.py"
 
-nerd-fonts/font-patcher -c -out "dist" "dist/MomiageMono-Regular.ttf"
-nerd-fonts/font-patcher -c -out "dist" "dist/MomiageMono-Italic.ttf"
-nerd-fonts/font-patcher -c -out "dist" "dist/MomiageMono-Bold.ttf"
-nerd-fonts/font-patcher -c -out "dist" "dist/MomiageMono-BoldItalic.ttf"
-mv "dist/Momiage Mono Regular Nerd Font Complete.ttf" "dist/MomiageMono-Regular-NerdFont.ttf"
-mv "dist/Momiage Mono Italic Nerd Font Complete.ttf" "dist/MomiageMono-Italic-NerdFont.ttf"
-mv "dist/Momiage Mono Bold Nerd Font Complete.ttf" "dist/MomiageMono-Bold-NerdFont.ttf"
-mv "dist/Momiage Mono Bold Italic Nerd Font Complete.ttf" "dist/MomiageMono-BoldItalic-NerdFont.ttf"
+pushd "dist"
+    for filename in *.ttf; do
+        subfamily=$(echo $filename | sed -E 's/.+-(.+)\.ttf/\1/')
+        subfamily_separated=$(echo $subfamily | sed -E 's/([a-z])([A-Z])/\1 \2/g')
+
+        ../nerd-fonts/font-patcher -c $filename
+        mv "Momiage Mono ${subfamily_separated} Nerd Font Complete.ttf" "MomiageMono-${subfamily}-NerdFont.ttf"
+
+        zip "package.zip" $filename "MomiageMono-${subfamily}-NerdFont.ttf"
+    done
+
+    zip -j "package.zip" "../LICENSE"
+popd
